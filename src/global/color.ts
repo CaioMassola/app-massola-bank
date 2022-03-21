b2b058abe9df985ec623e0b32824acad5a788f18
CaioMassola <caio.massola@outlook.com>
2022-03-20 21:47:57 -0300

    fix: color dark mode

--- src/global/color.ts
+++ src/global/color.ts
@@ -1,12 +1,10 @@
-import { dark_mode } from "./enviroments";
-
 export const color = {
     light: {
         primary: '#0cbae0',
         secondary:'#010101'
     },
     dark: {
-        primary: '#000000',
-        secondary: '#1a1a1a'
+        primary: '#202125',
+        secondary: '#202125'
     }
 }
